7bb7107bac544db30f487bb97953f3c2d84c0579
Ethan Hill <ethanthill2020@gmail.com>
2026-05-12 22:53:55 -0500

    Initial Commit

---
+++ Recipe App/Recipe_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_AppApp.swift
+//  Recipe App
+//
+//  Created by Ethan  Hill on 5/12/26.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
